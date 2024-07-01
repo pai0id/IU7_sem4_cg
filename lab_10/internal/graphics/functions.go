195d96b87c094edcf213d2d2a4ef4607c6a97a98
pai0id <andrey.polyakov05@gmail.com>
2024-07-01 10:15:28 +0300

    cockballs

--- lab_10/internal/graphics/functions.go
+++ lab_10/internal/graphics/functions.go
@@ -7,23 +7,17 @@ var CurrFunc SurfaceEq
 type SurfaceEq func(x, z float64) float64
 
 var FuncArr = []SurfaceEq{
-	func(x, z float64) float64 { return math.Sin(x) * math.Sin(z) },
-	func(x, z float64) float64 { return math.Sin(math.Cos(x)) * math.Sin(z) },
-	func(x, z float64) float64 { return math.Cos(x) * z / 3 },
-	func(x, z float64) float64 { return math.Sqrt(x*x + z*z) },
-	func(x, z float64) float64 { return x*x - z*z },
-	func(x, z float64) float64 { return x*x + z*z },
 	func(x, z float64) float64 { return 0 },
+	func(x, z float64) float64 { return math.Sin(x) * math.Sin(z) },
+	func(x, z float64) float64 { return math.Sqrt(x*x + z*z) },
+	func(x, z float64) float64 { return x*x + z*z },
 }
 
 var FuncStrArr = []string{
-	"sin(x) * sin(z)",
-	"sin(cos(x)) * sin(z)",
-	"cos(x) * z / 3",
-	"sqrt(x^2 + z^2)",
-	"x^2 - z^2",
-	"x^2 + z^2",
 	"0",
+	"sin(x) * sin(z)",
+	"sqrt(x^2 + z^2)",
+	"x^2 + z^2",
 }
 
 func GetID(str string) SurfaceEq {
